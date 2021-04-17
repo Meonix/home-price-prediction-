5490f65f0aa2a31b0e472301707e2ec91050fd44
Meonix <duymark42@gmail.com>
2021-04-17 20:28:45 +0700

    adding svm

--- svm_regression.py
+++ svm_regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 data = pd.read_csv("new_dataset.csv")
 data.head()
 data = data.drop(columns='id')
@@ -15,7 +16,7 @@ X = data.drop(columns='price')
 #X = sc_X.fit_transform(X)
 #y = sc_y.fit_transform(y)
 
-regressor = MLPRegressor(random_state=0)
+regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
 
 y_pred = regressor.predict(np.array([3,2.56,1700,4252,3,0,0,4,6,1722,800,1922,0,98114,47.2323,-122.243]).reshape(1, 16))
